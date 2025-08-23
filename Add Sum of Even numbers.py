91de650446fa110b9827803617adace420951dd0
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-08-23 10:44:22 +0530

    Method 2 updated

--- Add Sum of Even numbers.py
+++ Add Sum of Even numbers.py
@@ -1,3 +1,4 @@
+#method 1 for user input
 numbers = input("Enter Numbers:")
 even_sum =0
 
@@ -6,4 +7,18 @@ for i in numbers:
     if num%2==0:
         even_sum+=num
 print("Sumof of Even numbers:",even_sum)
+
+#method 2 for Given input
+def even_sum(numbers):
+    total_num=0
+    for num in numbers:
+        if num%2==0:
+            total_num+=num
+    return total_num
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+print("sum of total numbers",even_sum(numbers))
+
+
+
         
