2328bc79032c993efe6bd8e652e2d82ad7a48525
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-08-24 23:02:17 +0530

    vowel seperator added

--- Vowel counter.py
+++ Vowel counter.py
@@ -13,3 +13,15 @@ letter = "hello"  #inbuilt word
 print("No of vowels in words:",Vowel_counter(letter))    #print of inbuilt string  
 
     
+#vowel seperator
+def vowel_seperator(string1):
+    vowel_words=[]
+    vowel="aeiouAEIOU"
+    for i in string1:
+        if i in vowel:
+            vowel_words+=i
+    return vowel_words
+
+string1 = input("Enter a word:")
+print("The vowel words in the word is :",vowel_seperator(string1)) 
+
