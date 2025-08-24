a44cd77d23f26b44e0f7287e2edc04a3fdd844dd
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-08-24 23:24:44 +0530

    word duplicates updated

---
+++ Character Frequency Detective.py
@@ -0,0 +1,10 @@
+#Duplicate word detector
+
+def duplicate_word_detctor(string):
+    if len(string) == len(set(string)):
+        return "No Duplicates"
+    else:
+        return "Duplicates found"
+
+string = input("Enter a string:")
+print("The result:",duplicate_word_detctor(string))
