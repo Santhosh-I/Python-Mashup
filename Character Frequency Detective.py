5b2d4beaca21f00d7a20d2c54d0f7ac797d72714
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-08-24 23:53:25 +0530

    30% completed

--- Character Frequency Detective.py
+++ Character Frequency Detective.py
@@ -10,3 +10,15 @@ string = input("Enter a string:")
 print("The result:",duplicate_word_detctor(string))
 
 
+#Character Frequency Detective
+
+def Character_Frequency_Detective(string1):
+    if len(string1)==len(set(string1)):
+        return "None"
+    else:
+        no_duplicates = list(set(string1))
+        return no_duplicates
+
+
+print("The result:",Character_Frequency_Detective(string))
+
