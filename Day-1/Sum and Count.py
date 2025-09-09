aadd6a43d7d7ee339fdca542e708e08c3a6d0e33
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-09 09:13:32 +0530

    completed

--- Day-1/Sum and Count.py
+++ Day-1/Sum and Count.py
@@ -1,6 +1,11 @@
-def Sum_and_Count(numbers):
-    numbers = []
-    return numbers
+def Sum_of_numbers(numbers):
+    Sum_of_numbers = sum(numbers)
+    return Sum_of_numbers
+
+def Count_of_numbers(numbers):
+    Count_of_numbers = len(numbers)
+    return Count_of_numbers
 
 numbers = [1,2,3]
-print("The sum is :",Sum_and_Count(numbers))
+print("The Sum is :",Sum_of_numbers(numbers))
+print("The Count is :",Count_of_numbers(numbers))
