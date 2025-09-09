4825a6ab89630464c1cb84ed052f3862d0de6dfe
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-09 21:30:03 +0530

    done

--- Day-1/Sum and Count.py
+++ Day-1/Sum and Count.py
@@ -7,5 +7,6 @@ def Count_of_numbers(numbers):
     return Count_of_numbers
 
 numbers = [1,2,3]
+print("Given Numbers:",numbers)
 print("The Sum is :",Sum_of_numbers(numbers))
 print("The Count is :",Count_of_numbers(numbers))
