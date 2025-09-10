cd62ad6f1fc605527bbd48361fad4da30e980cb8
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-10 09:50:34 +0530

    done

--- Extras/Guessing_Game.py
+++ Extras/Guessing_Game.py
@@ -1,4 +1,15 @@
 import random
 
 random_picker = random.randint(1,5)
-print(random_picker)
+
+print("Guess a number Between 1 to 10")
+
+while True:
+    guess = int(input("Enter the guessed number:"))
+
+    if guess == random_picker:
+        print("Your guess is correct!")
+        break
+
+    else:
+        print("Wrong try again")
