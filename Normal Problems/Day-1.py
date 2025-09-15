0a8dfea1268de1afdd43714ffb6100cc193efbcc
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-15 21:15:29 +0530

    Normal Constructor problem finished

---
+++ Normal Problems/Day-1.py
@@ -0,0 +1,9 @@
+name = "Santhosh"
+age = 18
+
+print(f"My name is {name} and my age is {age}") #formated string
+
+print("My name is {} and my age is {}".format(name,age)) #.format function
+
+print("My name is %s and my age is %d"%(name,age)) # % constructor
+
