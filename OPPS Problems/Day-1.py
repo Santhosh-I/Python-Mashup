67cfe5797ca5145f259214000fc046e7b3e7f017
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-15 22:04:45 +0530

    OOPS completed

---
+++ OPPS Problems/Day-1.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+    def display(self):
+        print(f"Student Name:{self.name}, Marks:{self.marks}")
+
+s1 = Student("Santhosh",100)
+s1.display()
