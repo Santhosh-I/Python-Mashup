669440589ce1f4fff4c92d40756b90d494c0919b
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-16 20:50:34 +0530

    partialy completed

--- DSA Problems/Day-2.py
+++ DSA Problems/Day-2.py
@@ -1,6 +1,9 @@
 List = list(map(int,input("Enter the list of numbers with space seperated:").split()))
 Target = int(input("Enter Targer value:"))
-if List[Target] in List:
-    print(List[Target])
+
+Length = len(List)
+
+if Target<Length:
+    print("Element",List[Target],"found at index",Target)
 else:
-    print("No Element Found")
+    print("Element Not Found")
