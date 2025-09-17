0dd4d3b8ab37f6aae56eb25ef6c6cd8b5ebdb24f
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-17 08:56:31 +0530

    secondary versio updated

--- OPPS Problems/Day-2.py
+++ OPPS Problems/Day-2.py
@@ -1,10 +1,13 @@
 class Calculator:
     def add(a,b):
-        print(a+b)
+        return(a+b)
     def subtract(a,b):
-        print(a-b)
+        return(a-b)
     def multiply(a,b):
-        print(a*b)
+        return(a*b)
     def divide(a,b):
-        print(a/b)
+        return(a/b)
 
+c = Calculator
+print(c.add(10,5))
+print(c.multiply(2,5))
