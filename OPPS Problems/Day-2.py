cb5756c2401f26ff27f0a637d0888562196cf45b
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-17 08:43:03 +0530

    initiated Day-2 oops

---
+++ OPPS Problems/Day-2.py
@@ -0,0 +1,10 @@
+class Calculator:
+    def add(a,b):
+        print(a+b)
+    def subtract(a,b):
+        print(a-b)
+    def multiply(a,b):
+        print(a*b)
+    def divide(a,b):
+        print(a/b)
+
